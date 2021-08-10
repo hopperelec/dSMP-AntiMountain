5e76e184734e2afff4f6e16004afa35e58e8fc6e
BuildTools <unconfigured@null.spigotmc.org>
2021-08-10 03:58:03 +0100

    - Refactored Main to AntiMountain

--- src/main/java/uk/co/hopperelec/mc/dsmpantimountain/AntiMountain.java
+++ src/main/java/uk/co/hopperelec/mc/dsmpantimountain/AntiMountain.java
@@ -13,7 +13,7 @@ import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class AntiMountain extends JavaPlugin implements Listener {
     ICachedBiomeProvider worldBiomes;
 
     @Override
